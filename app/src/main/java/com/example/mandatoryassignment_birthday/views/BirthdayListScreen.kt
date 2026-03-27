5bbb6b906d2dd6c559888502e936ae2a31e83e30
EmlShield <eml004@edu.zealand.dk>
2026-03-27 13:12:10 +0100

    Update BirthdayListScreen.kt

--- app/src/main/java/com/example/mandatoryassignment_birthday/views/BirthdayListScreen.kt
+++ app/src/main/java/com/example/mandatoryassignment_birthday/views/BirthdayListScreen.kt
@@ -39,7 +39,6 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.mandatoryassignment_birthday.data.model.Birthday
 import com.example.mandatoryassignment_birthday.data.model.SortOrder
